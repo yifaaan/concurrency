c6524bfccdfecb02391c32ae8a18b145ef61d185
yifaaan <0219lyf@gmail.com>
2024-09-11 19:51:27 +0800

    support dot_product

--- src/matrix.rs
+++ src/matrix.rs
@@ -36,9 +36,22 @@ where
     let mut c = vec![T::default(); a.row * b.col];
     for i in 0..a.row {
         for j in 0..b.col {
-            for k in 0..a.col {
-                c[i * b.col + j] += a.data[i * a.col + k] * b.data[k * b.col + j];
-            }
+            // c[i * b.col + j] += a.data[i * a.col + k] * b.data[k * b.col + j];
+
+            // a的行向量
+            let mut row_elem = vec![];
+
+            row_elem.extend_from_slice(&a.data[i * a.col..(i + 1) * a.col]);
+            // b的列向量
+            let mut col_elem = vec![];
+            col_elem.extend_from_slice(
+                &b.data[j..]
+                    .iter()
+                    .step_by(b.col)
+                    .copied()
+                    .collect::<Vec<_>>(),
+            );
+            c[i * b.col + j] += dot_product(row_elem, col_elem)?;
         }
     }
     Ok(Matrix {
@@ -48,6 +61,20 @@ where
     })
 }
 
+fn dot_product<T>(a: Vec<T>, b: Vec<T>) -> anyhow::Result<T>
+where
+    T: Default + Copy + Add<Output = T> + Mul<Output = T> + AddAssign,
+{
+    if a.len() != b.len() {
+        return Err(anyhow::anyhow!("Dot product error: a.len()!= b.len()"));
+    }
+    let mut sum = T::default();
+    for i in 0..a.len() {
+        sum += a[i] * b[i];
+    }
+    Ok(sum)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -55,17 +82,17 @@ mod tests {
     #[test]
     fn test_multiply() {
         let a = Matrix {
-            data: vec![1, 2, 3, 4, 5, 6, 7, 8, 9],
+            data: vec![2, 0, 1, 3, 0, 0, 5, 1, 1],
             row: 3,
             col: 3,
         };
         let b = Matrix {
-            data: vec![9, 8, 7, 6, 5, 4, 3, 2, 1],
+            data: vec![1, 0, 1, 1, 2, 1, 1, 1, 0],
             row: 3,
             col: 3,
         };
         let expected = Matrix {
-            data: vec![30, 24, 18, 84, 69, 54, 138, 114, 90],
+            data: vec![3, 1, 2, 3, 0, 3, 7, 3, 6],
             row: 3,
             col: 3,
         };
