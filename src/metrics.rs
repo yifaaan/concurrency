bbb54074ca60e40075ac124c6293144f17c47646
yifaaan <0219lyf@gmail.com>
2024-09-12 10:10:00 +0800

    use DashMap

--- src/metrics.rs
+++ src/metrics.rs
@@ -1,24 +1,21 @@
-use std::collections::HashMap;
+use dashmap::DashMap;
 use std::fmt::Display;
 use std::sync::Arc;
-use std::sync::RwLock;
 
 #[derive(Debug, Clone)]
 pub struct Metrics {
-    data: Arc<RwLock<HashMap<String, i64>>>,
+    data: Arc<DashMap<String, i64>>,
 }
 
 impl Metrics {
     pub fn new() -> Self {
         Self {
-            data: Arc::new(RwLock::new(HashMap::new())),
+            data: Arc::new(DashMap::new()),
         }
     }
 
     pub fn inc(&self, key: impl Into<String>) -> anyhow::Result<()> {
         self.data
-            .write()
-            .map_err(|e| anyhow::anyhow!("Unlock error: {}", e))?
             .entry(key.into())
             .and_modify(|e| *e += 1)
             .or_insert(0);
@@ -27,30 +24,19 @@ impl Metrics {
 
     pub fn dec(&self, key: impl Into<String>) -> anyhow::Result<()> {
         self.data
-            .write()
-            .map_err(|e| anyhow::anyhow!("Unlock error: {}", e))?
             .entry(key.into())
             .and_modify(|e| *e -= 1)
             .or_insert(0);
         Ok(())
-    }
-
-    pub fn snapshot(&self) -> anyhow::Result<HashMap<String, i64>> {
-        Ok(self
-            .data
-            .write()
-            .map_err(|e| anyhow::anyhow!("Unlock error: {}", e))?
-            .clone())
     }
 }
 
 impl Display for Metrics {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         // write!(f, "{")?;
-        let data = self.data.read().map_err(|_| std::fmt::Error {})?;
 
-        for (key, value) in data.iter() {
-            write!(f, "{}: {},", key, value)?;
+        for entry in self.data.iter() {
+            write!(f, "{}: {},", entry.key(), entry.value())?;
         }
         write!(f, "\n")?;
         Ok(())
